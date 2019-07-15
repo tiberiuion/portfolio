fea0dec092f34741fac97d7e0ebad64f75164cbd
Tiberiu Ion <tion@Tiberius-MBP.lan>
2019-07-15 21:59:30 +0100

    content amendments

--- src/index.js
+++ src/index.js
@@ -8,14 +8,9 @@ function component() {
     element.innerHTML = `
     <div class="content">
       <h1 class="title">Tiberiu Ion</h1>
-      <p class="copy__secondary">Digital designer and wannabe web developer</p>
-      <p class="copy">Currently, I’m a Creative Designer at UCAS. I develop HTML email templates and the software behind interactive kiosks deployed at exhibitions.
-      </p>
-
-      <p class="copy">
-        If you fancy talking about design, web development or cats, <a href="mailto:tiberiu.vi@gmail.com"> drop me a line</a>.
-      </p>
+      <p class="copy__secondary">Digital designer and front end developer in training</p>
       <ul class="socials">
+        <li class="socials__item"><a href="mailto:tiberiu.vi@gmail.com">Email</a></li>
         <li class="socials__item"><a href="https://twitter.com/">Twitter</a></li>
         <li class="socials__item"><a href="https://www.instagram.com/tiberiuion91/">Instagram</a></li>
         <li class="socials__item"><a href="https://github.com/tiberiuion">Github</a></li>
